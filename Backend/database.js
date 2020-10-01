9a6cd8af544c0a00566c31cdd9b32f9ccb949fb9
Camilo318 <camiloa0318@gmail.com>
2020-10-01 18:50:18 -0500

    Update mongo uri

--- Backend/database.js
+++ Backend/database.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
-const MONGO_URI = 'mongodb+srv://fullstack-books-db:dPsDiGpVLpYP1kZv@cluster0.lcq17.mongodb.net/test'
 
 mongoose.set('useUnifiedTopology', true)
-mongoose.connect(MONGO_URI, {useNewUrlParser: true})
-.then(db => console.log('DB is connected'))
-.catch(err => console.log(err.message))
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+})
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.log(err))
