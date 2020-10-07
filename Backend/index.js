0209ea1c3705c5d4561993540544ed7a5167dd28
Camilo318 <camiloa0318@gmail.com>
2020-10-06 19:23:17 -0500

    Add cors, update multer

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ const fs = require('fs').promises
 const morgan = require('morgan')
 const multer = require('multer')
 const path = require('path')
+const cors = require('cors')
 
 //Init
 const app = express()
@@ -20,10 +21,10 @@ const storage = multer.diskStorage({
         cb(null, new Date().getTime() + path.extname(file.originalname))
     }
 })
-app.use(multer({storage}).single())
+app.use(multer({storage}).single('image'))
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
-
+app.use(cors())
 //Routes
 app.use('/api/books/', require('./routes/books'))
 
